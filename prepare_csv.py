7a46a11119d576502293bcb42716353d356e1d08
Jitesh Jain <jitesh.jj2@gmail.com>
2020-08-31 00:25:55 +0530

    Correct Frequnecy and Rececny calculation for data

--- prepare_csv.py
+++ prepare_csv.py
@@ -8,34 +8,41 @@ import codecs
 def minDistance(arr): 
       
     mp = {}
-    dist = {} 
+    indexes = {}
+    distances = [] 
   
-
+    minDistance = float('inf')
     for i in range(len(arr)): 
 
-        if arr[i] not in mp.keys(): 
-            mp[arr[i]] = i
-            dist[arr[i]] = float('inf') 
-        else:
-            dist[arr[i]] = min(dist[arr[i]],i-mp[arr[i]])
-            mp[arr[i]] = i
-
-    return dist
+        if arr[i] not in indexes.keys(): 
+            mp[arr[i]] = [-1,i]
+            indexes[arr[i]] = [i]
+        else: 
+            indexes[arr[i]].append(i)
+            mp[arr[i]] = [mp[arr[i]][1],i]
+            dist = i - mp[arr[i]][0]
+            distances[mp[arr[i]][0]] = dist
+        distances.append(minDistance)
+    return distances
 
 def Freq(arr): 
        
-    mp = {} 
+    indexes = {}
+    frequencies = [1]*len(arr)
 
     for i in range(len(arr)): 
-  
 
-        if arr[i] not in mp.keys(): 
-            mp[arr[i]] = 1 
-  
+        if arr[i] not in indexes.keys(): 
+            indexes[arr[i]] = [i]
         else: 
-            mp[arr[i]] += 1 
-  
-    return mp  
+            indexes[arr[i]].append(i)
+    for address in arr:
+        if len(indexes[address]) >=1:
+            freq = len(indexes[address]) - 1
+            frequencies[indexes[address][0]] = freq
+            indexes[address].pop(0)
+
+    return frequencies
 
 def main(args):
     count = 0
@@ -45,43 +52,39 @@ def main(args):
     distances = []
     
 # For data from txt file    
-    # with codecs.open(args.r, 'r', encoding='utf-8',errors='ignore') as file:
-    #     inputFile=file.readlines()
-    # for line in tqdm(inputFile):
-    #     item = line.split(" ")
-    #     if len(item) is 4:
-    #         page_counters.append(item[1])
-    #         addresses.append(item[2])
-    #     else:
-    #         print('---------------------------')
-    #         print(len(item))
-    #         print(item)
-    #         print('---------------------------')
-    #     count+=1
-    # print('---------------------------')
-    # print('Count: {}'.format(count))
-    # print('---------------------------')
-
-# For data fri]om csv file
-    with open(args.r,'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            count+=1
-            if count == 1:
-                continue
-            else:
-                page_counters.append(row[1])
-                addresses.append(row[2])
+    with codecs.open(args.r, 'r', encoding='utf-8',errors='ignore') as file:
+        inputFile=file.readlines()
+    for line in tqdm(inputFile):
+        item = line.split(" ")
+        if len(item) is 3:
+            page_counters.append(item[0].split(':')[0])
+            addresses.append(item[2])
+        else:
+            print('---------------------------')
+            print(len(item))
+            print(item)
+            print('---------------------------')
+        count+=1
     print('---------------------------')
     print('Count: {}'.format(count))
     print('---------------------------')
 
-    address_freq = Freq(addresses)
-    address_dist = minDistance(addresses)
+# # For data fri]om csv file
+#     with open(args.r,'r') as file:
+#         reader = csv.reader(file)
+#         for row in reader:
+#             count+=1
+#             if count == 1:
+#                 continue
+#             else:
+#                 page_counters.append(row[1])
+#                 addresses.append(row[2])
+#     print('---------------------------')
+#     print('Count: {}'.format(count))
+#     print('---------------------------')
 
-    for address in addresses:
-        frequencies.append(address_freq[address])
-        distances.append(address_dist[address])
+    frequencies = Freq(addresses)
+    distances = minDistance(addresses)
     
     data = {'PC': page_counters, 'Address': addresses, 'Frequency': frequencies, 'Recency': distances}
 
