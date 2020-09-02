12f28f7fbe58bbacafec8f5eba7eb2f455a259de
Jitesh Jain <jitesh.jj2@gmail.com>
2020-09-03 00:34:35 +0530

    Add loop through all files in a dir automation

--- get_misses.py
+++ get_misses.py
@@ -5,6 +5,20 @@ import pandas as pd
 from tqdm import tqdm
 import codecs
 from collections import deque, defaultdict
+import os
+from glob import glob
+
+def get_files(p):
+    
+    PATH = p
+    csvs = []
+    files_list = [files for path, subdir, files in os.walk(PATH)]
+    for files in files_list:
+        for file in files:
+            csvs.append(file)
+    csv_files = [p+ '/' +file.split('.csv')[0] for file in csvs]
+    
+    return csv_files
 
 def LRU(blocktrace, frame):
     
@@ -83,63 +97,68 @@ def LFU(blocktrace, frame):
 
 
 def main(args):
-    count = 0
-    addresses = []
-    lru_misses = []
-    lfu_misses = []
     
-# # For data from txt file    
-#     with codecs.open(args.r, 'r', encoding='utf-8',errors='ignore') as file:
-#         inputFile=file.readlines()
-#     for line in tqdm(inputFile):
-#         item = line.split(" ")
-#         if len(item) is 3:
-#             page_counters.append(item[0].split(':')[0])
-#             addresses.append(item[2])
-#         else:
-#             print('---------------------------')
-#             print(len(item))
-#             print(item)
-#             print('---------------------------')
-#         count+=1
-#     print('---------------------------')
-#     print('Count: {}'.format(count))
-#     print('---------------------------')
+    files = get_files(args.r)
 
-# For data fri]om csv file
-    with open(args.r+'.csv','r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            count+=1
-            if count == 1:
-                print(row)
-                continue
-            else:
-                if count == 2:
+    for f in files:
+
+        count = 0
+        addresses = []
+        lru_misses = []
+        lfu_misses = []
+
+    # # For data from txt file    
+    #     with codecs.open(args.r, 'r', encoding='utf-8',errors='ignore') as file:
+    #         inputFile=file.readlines()
+    #     for line in tqdm(inputFile):
+    #         item = line.split(" ")
+    #         if len(item) is 3:
+    #             page_counters.append(item[0].split(':')[0])
+    #             addresses.append(item[2])
+    #         else:
+    #             print('---------------------------')
+    #             print(len(item))
+    #             print(item)
+    #             print('---------------------------')
+    #         count+=1
+    #     print('---------------------------')
+    #     print('Count: {}'.format(count))
+    #     print('---------------------------')
+
+    # For data fri]om csv file
+        with open(f+'.csv','r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                count+=1
+                if count == 1:
                     print(row)
-                addresses.append(row[2])
-    print('---------------------------')
-    print('Count: {}'.format(count))
-    print('---------------------------')
+                    continue
+                else:
+                    if count == 2:
+                        print(row)
+                    addresses.append(row[2])
+        print('---------------------------')
+        print('Count: {}'.format(count))
+        print('---------------------------')
 
-    lru_misses = LRU(addresses,32)
-    lfu_misses = LFU(addresses,32)
-    
-    data_lru = {'LRU Miss Address': lru_misses}
+        lru_misses = LRU(addresses,32)
+        lfu_misses = LFU(addresses,32)
 
-    new_df_lru = pd.DataFrame(data_lru,columns=['LRU Miss Address'])
-    new_df_lru.to_csv(args.r+'_lru_misses.csv')
+        data_lru = {'LRU Miss Address': lru_misses}
 
-    data_lfu = {'LFU Miss Address': lfu_misses}
+        new_df_lru = pd.DataFrame(data_lru,columns=['LRU Miss Address'])
+        new_df_lru.to_csv(args.r+'_lru_misses.csv')
 
-    new_df_lfu = pd.DataFrame(data_lfu,columns=['LFU Miss Address'])
-    new_df_lfu.to_csv(args.r+'_lfu_misses.csv')
+        data_lfu = {'LFU Miss Address': lfu_misses}
+
+        new_df_lfu = pd.DataFrame(data_lfu,columns=['LFU Miss Address'])
+        new_df_lfu.to_csv(f+'_lfu_misses.csv')
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Train_overexposure")
     parser.add_argument("--r", required=True,
-    help="file name without extension")
+    help="path to directory containing the files")
     args =  parser.parse_args()
 
     main(args)
